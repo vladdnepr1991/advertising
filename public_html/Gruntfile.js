e930537b80b6d99c78ae03d888c3b4dca4433526
vladdnepr1991 <vladdnepr1991@gmail.com>
2016-11-05 01:15:13 +0200

    grunt tutorial

---
+++ public_html/Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		less: {
+			build: {
+				files: {
+					'css/test.css': 'css/test.less'
+				}
+			}
+		},
+		uglify: {
+			options: {
+				banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+			},
+			build: {
+				files: {
+					'dist/js/main.min.js': [
+						'scripts/slider.js', 
+						'scripts/screens.js', 
+						'scripts/screenEffects.js', 
+						'scripts/videoAdvertising.js', 
+						'scripts/main.js'
+					]
+				}
+			}
+		},
+		watch: {
+			files: 'css/test.less',			
+			tasks: ["less"]
+		}	
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-less');
+};
